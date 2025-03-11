9b5cde235f71241fba9fcc82d61e78bfe9b3b116
Da-Weezyyy <luheinfrazenburg33@gmail.com>
2025-03-11 10:46:41 +0200

    move

---
+++ prjClasses2/src/main/java/com/mycompany/prjclasses2/PrjClasses2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prjclasses2;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class PrjClasses2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World this is my first github activity");
+        
+        newClass nc = new newClass();
+                
+         nc.helloClass();
+         
+         System.out.println(nc.strSecondClasssVar);
+    }
+}
